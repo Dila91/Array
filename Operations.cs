8189d6523954fbf25e4d85a46093916cf0f800c6
Dila91 <59875110+Dila91@users.noreply.github.com>
2020-01-29 11:44:55 +0100

    Initial commit

---
+++ Operations.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Array
+{
+    class Operations
+    {
+
+         public static void printArray(int i, int j)
+        {
+            int[] Arrlon = new int[i];
+            string[] Arrname = new string[j];
+           
+            
+            for (int n = 0; n < Arrlon.Length; n++)
+            {
+                Console.Write("Input the employers name:");
+                Arrname[n] = Console.ReadLine();
+                Console.Write("Input the implojers  lon :");
+                Arrlon[n] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+            }
+            Console.WriteLine("==============================================================");
+            Console.WriteLine();
+            Console.WriteLine("Employers Nam and Lon Tabell:");
+           
+
+            for (int n = 0; n < Arrlon.Length; n++)
+            {
+
+                Console.WriteLine(Arrname[n] + "\t" + Arrlon[n]);
+                Console.WriteLine();
+
+            }
+            
+
+         }
+            
+          
+}
+    }
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            //Operations arr1 = new Operations();
+            Console.WriteLine("Enter how many employers info`s you want to incode :");
+            int i = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter how many employers you want to incode : ");
+            int j = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Imployers names and lon:");
+            Console.WriteLine("====================================================");
+
+            //arr1.printArray(i,j);
+            Operations.printArray(i, j);
+
+            //Console.ReadKey();
+        }
+    }
+}
